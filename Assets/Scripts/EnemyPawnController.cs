2e472eace7cc5d98c1b105629e9e03ff503adc7c
Maiko Tuitupou <tuitupoumaiko@gmail.com>
2020-01-29 14:35:54 -0700

    Added skeleton for enemy

---
+++ Assets/Scripts/EnemyPawnController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPawnController : MonoBehaviour
+{
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleMovement();
+        HandleShooting();
+    }
+
+    private void HandleShooting()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void HandleMovement()
+    {
+        throw new NotImplementedException();
+    }
+}
